3922370b0b3013efbb23c04fd92ef818896771c9
mvavilin <realvavilin@yandex.ru>
2026-01-19 19:02:32 +0300

    chore: add automatic deployment of Vite projects to GitHub Pages

--- async-race/vite.config.js
+++ async-race/vite.config.js
@@ -6,6 +6,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 export default defineConfig({
+  base: '/mvavilin-JSFE2025Q3/',
   resolve: {
     alias: {
       '@pages': resolve(__dirname, 'src/pages'),
